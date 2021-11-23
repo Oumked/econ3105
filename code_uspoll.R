9a14c9e14f63f3f36f4604952c65501e98d0fea1
Elmeri Niemi <elmeri.niemi@gmail.com>
2021-11-23 09:49:45 +0800

    Initial commit

---
+++ code_uspoll.R
@@ -0,0 +1,14 @@
+getwd()
+
+# SET YOUR WORKING DIRECTORY
+setwd("/Users/elmeriniemi/Desktop/Programming/ECON_group")
+# SET YOUR WORKING DIRECTORY
+
+#check that the filename is correct
+uspoll_fp <- "pollution_us_2000_2016.csv"
+
+df_uspoll <- read.csv(uspoll_fp, sep = ",", header = 1)
+
+# Col information
+str(df_uspoll)
+head(df_uspoll)
